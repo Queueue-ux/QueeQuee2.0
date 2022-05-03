d8f53607c1080b87916e90ae643edd3e5aca3d95
Quinton <q@gmail.com>
2022-05-03 15:54:47 -0600

    added alias, added check for empty queue

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -3,11 +3,12 @@ const { MessageEmbed } = require('discord.js');
 const client = require('../../main');
 
 module.exports = {
-    name: 'pause',
+    name: 'queue',
+    aliases: ['q'],
     async execute(message, args) {
         const { member, channel, voiceChannel } = get_vars(message);
         const queue = await client.distube.getQueue(voiceChannel);
-
+        if (!queue) return message.channel.send('There is nothing in the queue right now!');
         if (!voiceChannel) {
             return message.reply('must be in a voice channel to use music commands');
         }
