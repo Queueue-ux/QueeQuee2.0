9a72d6a70551029314d970d5aaf03676356b548b
Quinton <q@gmail.com>
2022-05-03 16:03:02 -0600

    deleted tests

--- commands/test/s1.js
+++
@@ -1,8 +0,0 @@
-let { value } = require('./shared');
-module.exports = {
-	name: 's1',
-	async execute(message, args) {
-		await message.channel.send(`${value.x}`);
-        value.x += 1;
-	},
-};
--- commands/test/shared.js
+++
@@ -1,6 +0,0 @@
-class sh{
-    static x = 0;
-}
-module.exports = {
-    value: sh,
-};
