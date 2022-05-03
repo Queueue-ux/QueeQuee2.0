0f9281934a4e6491d811bfefe60515dd557a40da
Quinton <q@gmail.com>
2022-05-03 16:09:15 -0600

    added check so bot doesn't say goodbye to itself

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,10 @@
 const { spam_channel } = require('../config.json');
+const BOT_ID = '946542952946216960';
 module.exports = {
 name: 'voiceStateUpdate',
 async execute(before, after) {
     // user has left voice channel, send a message saying goodbye to them
-    if (!after.channel) {
+    if (!after.channel && after.member.id != BOT_ID) {
         for (const channel of before.channel.guild.channels.cache.values()) {
             if (channel.name == spam_channel) {
                 await channel.send(`See ya ${before.member.nickname}!`);
