b32de2a758ef31bcceddea3e098e71ae03d9c3ca
Quinton <q@gmail.com>
2022-05-03 15:51:47 -0600

    changed printout, added new alias

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -4,13 +4,13 @@ const client = require('../../main');
 
 module.exports = {
   name: 'filter',
-  aliases: ['filters'],
+  aliases: ['f'],
   async execute(message, args) {
     const queue = client.distube.getQueue(message);
     if (!queue) return message.channel.send('There is nothing in the queue right now!');
     if (args[0] === 'off' && queue.filters?.length) queue.setFilter(false);
     else if (Object.keys(client.distube.filters).includes(args[0])) queue.setFilter(args[0]);
-    else if (args[0]) return message.channel.send(`${client.emotes.error} | Not a valid filter`);
+    else if (args[0]) return message.channel.send('Not a valid filter');
     message.channel.send(`Current Queue Filter: \`${queue.filters.join(', ') || 'Off'}\``);
   },
 };
