3b55e841d2bea16188c160bea176de19acd31421
Quinton <q@gmail.com>
2022-06-08 19:25:15 -0600

    bot_id is now taken from the config.json file

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,18 +1,14 @@
-const { spam_channel } = require('../config.json');
-const BOT_ID = '946542952946216960';
+const { spam_channel, bot_id} = require('../config.json');
 module.exports = {
 name: 'voiceStateUpdate',
 async execute(before, after) {
     // user has left voice channel, send a message saying goodbye to them
-    if (!after.channel && after.member.id != BOT_ID) {
+    if (!after.channel && after.member.id != bot_id) {
         for (const channel of before.channel.guild.channels.cache.values()) {
             if (channel.name == spam_channel) {
                 await channel.send(`See ya ${before.member.nickname}!`);
             }
         }
-    }
-    // TODO: let bot leave from here
-    if (before.channel != after.channel) {
     }
 },
 };
