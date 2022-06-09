61e259ac52ddcef737dfb79455c67f1e490a627a
Quinton <q@gmail.com>
2022-06-08 19:14:44 -0600

    now properly uses the play function, instead of making its own play creating two nearly identical functions which was bad

--- commands/music/trap_remix.js
+++ commands/music/trap_remix.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const { get_vars } = require('./music_args_handling');
 const client = require('../../main');
+const play = require('./play');
 
 module.exports = {
     name: 'trapremix',
@@ -10,16 +11,6 @@ module.exports = {
     aliases: ['tr'],
     async execute(message, args) {
         args.push('trap remix');
-        const { member, channel, voiceChannel } = get_vars(message);
-
-        if (!voiceChannel) {
-            return message.reply('must be in a voice channel to use music commands');
-        }
-        if (args.length < 1) {
-            return message.reply('play requires argument of youtube/soundcloud link or query')
-        }
-        const search = args.join(' ');
-        await client.distube.play(voiceChannel, search, { textChannel: channel, member: member });
-        return message.reply({ content: 'Request received' });
+        play.execute(message, args);
     },
 };
