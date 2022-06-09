b0017f7a7015801544d53dd132ff38160ebe32e3
Quinton <q@gmail.com>
2022-06-08 19:24:50 -0600

    adding a file for stats to be collected and displayed to users

---
+++ commands/stats/stats.js
@@ -0,0 +1,6 @@
+/*
+Overview of module
+    - use of a database in my project
+    - store any type of data I want people to be able to see
+        - examples: time spent in channel, time spent afk lol, time spent actually speaking, messages sent in channel
+*/
