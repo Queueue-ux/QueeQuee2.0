de783b9156ef5dcb6349a32058badbcc96984651
Quinton <q@gmail.com>
2022-06-17 16:23:52 -0600

    added way to update total time in channel stat

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,23 @@
-const { spam_channel, bot_id} = require('../config.json');
+const { spam_channel } = require('../config.json');
+const { get_user_name } = require('../globals/parse_discord_objects');
+const { update_total_time } = require('../globals/stat_updates');
+const { get_current_time } = require('../globals/time_calculations');
+const client = require('../main');
+
 module.exports = {
 name: 'voiceStateUpdate',
 async execute(before, after) {
     // user has left voice channel, send a message saying goodbye to them
-    if (!after.channel && after.member.id != bot_id) {
+    if (!before.channel && !before.member.user.bot) {
+        const userName = get_user_name(before.member);
+        const user_obj = client.user_objects[userName];
+        user_obj.join_time = get_current_time();
+    }
+    if (!after.channel && !after.member.user.bot) {
+        const userName = get_user_name(before.member);
+        const user_obj = client.user_objects[userName];
+        user_obj.leave_time = get_current_time();
+        update_total_time(client, user_obj);
         for (const channel of before.channel.guild.channels.cache.values()) {
             if (channel.name == spam_channel) {
                 await channel.send(`See ya ${before.member.nickname}!`);
