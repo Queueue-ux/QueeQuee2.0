e3312f57d06df5d1ab48ede1aa75181cea24a930
Quinton <q@gmail.com>
2022-12-21 19:54:19 -0700

    changed stat output to look better

--- commands/stats/stats.js
+++ commands/stats/stats.js
@@ -10,6 +10,18 @@ const { get_user_stats } = require('../../globals/stat_updates');
 const client = require('../../main');
 const { get_user_name } = require('../../globals/parse_discord_objects');
 
+// get rid of stats people don't need to see, and do any necessary conversions
+function parse_stats(stat_obj) {
+  const return_obj = stat_obj;
+    delete return_obj.updatedAt;
+    delete return_obj.createdAt;
+    delete return_obj.id;
+    delete return_obj.user;
+    const temp = return_obj.seconds_spent_in_channel;
+    delete return_obj.seconds_spent_in_channel;
+    return_obj.hours_spent_in_voice = (temp / 3600).toFixed(2);
+    return return_obj;
+}
 
 module.exports = {
     name: 'stats',
@@ -18,10 +30,11 @@ module.exports = {
     usage: 'stats',
     aliases: ['s'],
     async execute(message, args) {
-        const stats = await get_user_stats(client, get_user_name(message.member));
+        const raw_stats = await get_user_stats(client, get_user_name(message.member));
+        const trimmed_stats = parse_stats(raw_stats);
         let stat_message = '';
-        for (const stat in stats) {
-            stat_message += stat + ': ' + stats[stat] + '\n';
+        for (const stat in trimmed_stats) {
+            stat_message += stat + ': ' + trimmed_stats[stat] + '\n';
         }
 
         return message.author.send(stat_message);
