7a9f8541dedc9dbf5ae8b4233925e4944af87166
Quinton <q@gmail.com>
2022-12-21 19:54:53 -0700

    adding way to increase Quee coin based on internet message

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,23 +1,45 @@
-const { prefix } = require('../config.json');
+const { prefix, web_data_channel, web_data_bot_username } = require('../config.json');
 const { MessageEmbed } = require('discord.js');
 const { emit_error } = require('../globals/error_output');
 const { get_user_name } = require('../globals/parse_discord_objects');
 const { update_total_messages } = require('../globals/stat_updates');
+const { increment } = require('../globals/database_commands');
 const client_obj = require('../main');
 module.exports = {
 name: 'messageCreate',
 async execute(message) {
-  // const message = client.content;
-  // Ignore all bots
   const client = message.client;
+
+  // ////////////////////////////////
+  // Stat updates are all done here
+  // ///////////////////////////////
+
+  // sub-section for updates specifically from the queequee-web-data channel
+  const channel_name = message.channel.name;
+
+  // NOTE: we only want bots here
+  if (channel_name == web_data_channel && message.author.bot && message.author.username == web_data_bot_username) {
+    // name scheme: {user_name}-{table}-{option}-{option-args}...
+    // options: coin - args: amount_to_increase
+    const arg_split = message.content.trim().split('-');
+
+    const table_user_name = arg_split[0];
+    const table = client.tables[arg_split[1]];
+
+    if (arg_split[2] == 'coin') {
+      increment(table, { quee_coin: parseFloat(arg_split[3]) }, { user: table_user_name });
+    }
+
+  }
+
+  // NOTE: everything below here cannot have bots
   if (message.author.bot) return;
 
-  //////////////////////////////////
-  // Stat updates are all done here
-  /////////////////////////////////
+  // regular stat update, just shows total messages
   const name = get_user_name(message.member);
   update_total_messages(client_obj, client_obj.user_objects[name]);
-  //////////////////////////
+
+  // ////////////////////////
 
   // Ignore messages not starting with the prefix (in config.json)
   if (message.content.indexOf(prefix) !== 0) {
